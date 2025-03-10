3d72ecaaa2832a3bcd2f8944c65a0fb98d8bdbdf
nadiamariaotieno <166137906+nadiamariaotieno@users.noreply.github.com>
2025-03-10 13:54:50 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+let student = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 21,
+  course: "Computer Science"
+};
+
+// Logging the 'student' object to the console
+console.log(student);
+
+// Accessing properties of the 'student' object
+console.log("First Name: " + student.firstName);
+console.log("Last Name: " + student.lastName);
+console.log("Age: " + student.age);
+console.log("Course: " + student.course);
